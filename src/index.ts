1d787dd67a9cc8213469e4ebf3a0c5a530aa47a7
Taras Byalyk <byalykt@hotmail.com>
2024-02-14 17:52:06 +0100

    Change to https

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
-const express = require('express');
-const app = express();
+const https = require('https');
+const fs = require('fs');
+const express = require('express'); // Import Express
 
-app.get('/', (req: any, res: any) => res.send('Hello True Shuffle!'));
+const app = express(); // Create an Express application
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+// Define the endpoints
+app.get('/', (req:any, res:any) => {
+    res.send('Hello World');
 });
+
+app.get('/api', (req:any, res:any) => {
+    res.send('Hell');
+});
+
+const privateKey = fs.readFileSync('~/etc/nginx/ssl/local-server.key', 'utf8');
+const certificate = fs.readFileSync('~/etc/nginx/ssl/local-server.crt', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+};
+
+// Create the HTTPS server using the Express app
+const httpsServer = https.createServer(credentials, app);
+
+// Listen on a port
+httpsServer.listen(3000, () => {
+    console.log('HTTPS Server running on port 3000');
+});
+
+// const express = require('express');
+// const https = require('https');
+// const fs = require('fs');
+// const app = express();
+
+// app.get('/', (req: any, res: any) => res.send('Hello True Shuffle!'));
+
+// const options = {
+//     hostname: 'api.example.com',
+//     key: fs.readFileSync('~/etc/nginx/ssl/local-server.key'), // replace it with your key path
+//     cert: fs.readFileSync('~/etc/nginx/ssl/local-server.crt'), // replace it with your certificate path
+// }
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
