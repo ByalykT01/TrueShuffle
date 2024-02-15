cc7b1a6a7292a1b01effa08365845a6dc198d904
Exclearf <nboholii@gmail.com>
2024-02-15 20:12:22 +0100

    Fix

--- src/index.ts
+++ src/index.ts
@@ -58,27 +58,29 @@ app.post("/login", (req: any, res: any) => {
     clientId: "77f685e7f75347a08e71369bd8eef061",
     clientSecret: "7522dff5768a4935aa862b365a33bb7a",
   });
-  spotifyApi
-    .authorizationCodeGrant(req.body.code)
-    .then((data: any) => {
-      // Set the access token on the API object to use it in later calls
-      console.log("-----authorizationCodeGrant------");
-      console.log(`accessToken: ${data.body.access_token}`);
-      console.log(`refreshToken: ${data.body.refreh_token}`);
-      console.log(`expiresIn: ${data.body.expires_in}`);
-      console.log("---------------------------------");
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refreh_token,
-        expiresIn: data.body.expires_in,
+  if (req.body.code) {
+    spotifyApi
+      .authorizationCodeGrant(req.body.code)
+      .then((data: any) => {
+        // Set the access token on the API object to use it in later calls
+        console.log("-----authorizationCodeGrant------");
+        console.log(`accessToken: ${data.body.access_token}`);
+        console.log(`refreshToken: ${data.body.refreh_token}`);
+        console.log(`expiresIn: ${data.body.expires_in}`);
+        console.log("---------------------------------");
+        res.json({
+          accessToken: data.body.access_token,
+          refreshToken: data.body.refreh_token,
+          expiresIn: data.body.expires_in,
+        });
+        console.log(res.json);
+        console.log("END OF FUNC");
+      })
+      .catch((err: any) => {
+        console.log("WHY DID I BREAK??");
+        console.log(err);
       });
-      console.log(res.json);
-      console.log("END OF FUNC");
-    })
-    .catch((err: any) => {
-      console.log("WHY DID I BREAK??");
-      console.log(err);
-    });
+  }
 });
 
 // HTTPS server
