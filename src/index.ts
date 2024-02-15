cf5f029c3039ccad1dd88638591d3be7135cdaa2
Exclearf <nboholii@gmail.com>
2024-02-15 01:13:52 +0100

    Started the implementation of the authentication

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,56 @@
-const https = require('https');
-const fs = require('fs');
-const express = require('express'); // Import Express
+const https = require("https");
+const fs = require("fs");
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
 
-const app = express(); // Create an Express application
-
-// Define the endpoints
-app.get('/', (req:any, res:any) => {
-    res.send('Hello World');
-});
-
-app.get('/api', (req:any, res:any) => {
-    res.send('Hell');
-});
-
-const privateKey = fs.readFileSync('/etc/nginx/ssl/local-server.key', 'utf8');
-const certificate = fs.readFileSync('/etc/nginx/ssl/local-server.crt', 'utf8');
+// SSL
+const privateKey = fs.readFileSync("/etc/nginx/ssl/local-server.key", "utf8");
+const certificate = fs.readFileSync("/etc/nginx/ssl/local-server.crt", "utf8");
 
 const credentials = {
-    key: privateKey,
-    cert: certificate,
+  key: privateKey,
+  cert: certificate,
 };
 
-// Create the HTTPS server using the Express app
-const httpsServer = https.createServer(credentials, app);
-
-// Listen on a port
-const PORT = process.env.PORT || 3000;
-httpsServer.listen(PORT, () => {
-    console.log('HTTPS Server running on port 3000');
-}).on('error', (err: any) => {
-    console.error('Server failed to start:', err);
+// Endpoints
+const app = express();
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World");
 });
 
-// const express = require('express');
-// const https = require('https');
-// const fs = require('fs');
-// const app = express();
+app.get("/api", (req: any, res: any) => {
+  res.send("Hell");
+});
 
-// app.get('/', (req: any, res: any) => res.send('Hello True Shuffle!'));
+app.get("/login", (req: any, res: any) => {
+    const code  = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '77f685e7f75347a08e71369bd8eef061',
+        clientSecret: '7522dff5768a4935aa862b365a33bb7a'
+    });
 
-// const options = {
-//     hostname: 'api.example.com',
-//     key: fs.readFileSync('~/etc/nginx/ssl/local-server.key'), // replace it with your key path
-//     cert: fs.readFileSync('~/etc/nginx/ssl/local-server.crt'), // replace it with your certificate path
-// }
+    spotifyApi.authorizationCodeGrant(code).then((data: any) => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refreh_token,
+            expiresIn: data.body.expires_in
+        }).cathc(() => {
+            res.sendStatus(404)
+        })
+    })
+  res.send("Hello World");
+});
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+// HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+// Listen
+const PORT = process.env.PORT || 3000;
+httpsServer
+  .listen(PORT, () => {
+    console.log("HTTPS Server running on port 3000");
+  })
+  .on("error", (err: any) => {
+    console.error("Server failed to start:", err);
+  });
