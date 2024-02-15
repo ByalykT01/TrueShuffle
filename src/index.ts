ef268747adc7093b5a959fa0b3cc7b5e34ab604c
Exclearf <nboholii@gmail.com>
2024-02-15 22:04:31 +0100

    Removed ability to use 1 key twice in a short span

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// Terminate duplicates
+const codes: any = new Set([1, 2, 3]);
+const insertCode = (code: string): boolean => {
+  if (codes.has(code)) return false;
+  codes.add(code);
+  setTimeout(() => codes.delete(codes), 5000);
+  return true;
+};
+
 // SSL
 const privateKey = fs.readFileSync("/etc/nginx/ssl/local-server.key", "utf8");
 const certificate = fs.readFileSync("/etc/nginx/ssl/local-server.crt", "utf8");
@@ -58,33 +67,29 @@ app.post("/refresh", (req: any, res: any) => {
 });
 
 app.post("/login", (req: any, res: any) => {
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "https://encape.me",
-    clientId: "77f685e7f75347a08e71369bd8eef061",
-    clientSecret: "7522dff5768a4935aa862b365a33bb7a",
-  });
-  console.log(`________${req.body.code}________`);
-  spotifyApi
-    .authorizationCodeGrant(req.body.code)
-    .then((data: any) => {
-      // Set the access token on the API object to use it in later calls
-      console.log("-----authorizationCodeGrant------");
-      console.log(`accessToken: ${data.body.access_token}`);
-      console.log(`refreshToken: ${data.body.refreh_token}`);
-      console.log(`expiresIn: ${data.body.expires_in}`);
-      console.log("---------------------------------");
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refreh_token,
-        expiresIn: data.body.expires_in,
-      });
-      console.log(res.json);
-      console.log("END OF FUNC");
-    })
-    .catch((err: any) => {
-      console.log("WHY DID I BREAK??");
-      console.log(err);
+  let code = req.body.code;
+  if (insertCode(code)) {
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: "https://encape.me",
+      clientId: "77f685e7f75347a08e71369bd8eef061",
+      clientSecret: "7522dff5768a4935aa862b365a33bb7a",
     });
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then((data: any) => {
+        res.json({
+          accessToken: data.body.access_token,
+          refreshToken: data.body.refreh_token,
+          expiresIn: data.body.expires_in,
+        });
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  } else {
+    res.sendStatus(200);
+  }
 });
 
 // HTTPS server
